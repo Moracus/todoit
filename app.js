8dfc0c476fc8bd794ff2bd848f2315db3d1cd251
harsh sharma <hsharshsharma370@gmail.com>
2023-11-16 20:13:49 +0530

    fixed task deletion bug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function addTask() {
     newLi.innerHTML = `
         <div class="input-group mb-3">
             <div class="input-group-text">
-                <input class="form-check-input mt-0" type="checkbox" onclick="check()" value="" aria-label="Checkbox for following text input">
+                <input class="form-check-input mt-0" type="checkbox"  value="" aria-label="Checkbox for following text input">
             </div>
             <input type="text" class="form-control" aria-label="Text input with checkbox" placeholder="New Task">
         </div>
@@ -21,6 +21,22 @@ function addTask() {
     inputText.focus();
 
 }
+const taskContainer = document.querySelector(".task-container")
+taskContainer.addEventListener('change', (e) => {
+    const checkbox = e.target;
+    if (checkbox.classList.contains('form-check-input') && checkbox.checked) {
+        // Find the closest li element containing the checked checkbox
+        let liToRemove = checkbox.closest('li');
+
+        // Check if the checkbox is within an li element
+        if (liToRemove) {
+            // Remove the li element from the taskList
+            taskList.removeChild(liToRemove);
+        }
+    }
+})
+
+
 function check() {
     let checkbox = document.querySelector(".form-check-input");
     const taskList = document.getElementById('taskList');
