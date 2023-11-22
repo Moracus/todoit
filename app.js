66567e2de78627d9175ad5e12e0b5feba52a3d7a
Moracus <hsharshsharma370@gmail.com>
2023-11-22 20:22:50 +0530

    added local storage and fixed bugs

--- app.js
+++ app.js
@@ -1,37 +1,96 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    displayTasks(savedTasks)
+
+})
+const taskContainer = document.querySelector(".task-container");
+taskContainer.addEventListener('keyup',(e)=>{
+    if(e.key==="Enter"&& e.target.classList.contains('task-box')){
+        addTask();
+    }
+})
 function addTask() {
-    const taskList = document.getElementById('taskList');
-
-    // Create a new li element
+    let noTasks = document.querySelector(".noTasks")
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const taskList = document.querySelector('#taskList');//ul
+    noTasks.style.display = 'none';
     const newLi = document.createElement('li');
-
-    // Set the innerHTML with the same structure as last li
-    newLi.innerHTML = `
-        <div class="input-group mb-3">
-            <div class="input-group-text">
-                <input class="form-check-input mt-0" type="checkbox"  value="" aria-label="Checkbox for following text input">
-            </div>
-            <input type="text" class="form-control" aria-label="Text input with checkbox" placeholder="New Task">
+    newLi.innerHTML = `<div class="input-group mb-3">
+        <div class="input-group-text">
+            <input class="form-check-input mt-0" type="checkbox" value=""
+                aria-label="Checkbox for following text input">
         </div>
-    `;
-    taskList.appendChild(newLi);
-    // Get the input element within the new li
-    const inputText = newLi.querySelector('.form-control');
+        <input type="text" class="form-control task-box" aria-label="Text input with checkbox"
+            placeholder="New Task">
+        </div>`
+        // Get the input element within the new li
 
     // Set focus on the input element
-    inputText.focus();
+
+    taskList.appendChild(newLi)
+    const taskBox = newLi.querySelector('.task-box');
+    taskBox.focus();
+
+
+    const inputTextList = document.querySelectorAll('.task-box')
+    let requiredIndex = inputTextList.length - 2;
+    if(inputTextList[requiredIndex]){
+        let toPush = inputTextList[requiredIndex].value;
+        if(tasks[tasks.length-1]!==toPush)
+        tasks.push(toPush);
+
+    }
+    // save the updated array to localstorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
 }
-const taskContainer = document.querySelector(".task-container")
+
+// deletion
+
 taskContainer.addEventListener('change', (e) => {
     const checkbox = e.target;
+    taskList = document.querySelector('#taskList');
+    // console.log(checkbox);
     if (checkbox.classList.contains('form-check-input') && checkbox.checked) {
         // Find the closest li element containing the checked checkbox
         let liToRemove = checkbox.closest('li');
-
+        let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
         // Check if the checkbox is within an li element
         if (liToRemove) {
             // Remove the li element from the taskList
+            tasks = tasks.filter((task) => {
+                return task !== liToRemove.querySelector('.task-box').value.trim();
+            })
+            console.log(tasks)
+            // save the updated task array
+            localStorage.setItem('tasks', JSON.stringify(tasks));
             taskList.removeChild(liToRemove);
         }
     }
 })
+function displayTasks(tasks) {
+    let noTasks = document.querySelector(".noTasks")
+    const taskList = document.querySelector('#taskList');
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        // console.log(task);
+        li.innerHTML = `
+        <div class="input-group mb-3">
+            <div class="input-group-text">
+                <input class="form-check-input mt-0" type="checkbox"  value="" aria-label="Checkbox for following text input">
+            </div>
+            <input type="text" class="form-control task-box" aria-label="Text input with checkbox" placeholder="New Task"
+            value = "${task}">
+        </div>
+    `;
+
+        taskList.appendChild(li);
+    });
+    if(tasks.length===0){
+        noTasks.style.display = 'block';
+    }
+    else{
+        noTasks.style.display = 'none';
+    }
+}
